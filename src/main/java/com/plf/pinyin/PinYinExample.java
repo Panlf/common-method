c70ed2b5331b6f9ebe3e471e888cf49d9b8237d4
Panlf <liangfeng_pan@163.com>
2017-06-08 22:25:43 +0800

    format

--- src/main/java/com/plf/pinyin/PinYinExample.java
+++ src/main/java/com/plf/pinyin/PinYinExample.java
@@ -3,6 +3,11 @@ package com.plf.pinyin;
 import org.junit.Test;
 
 import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
 /**
  * 拼音Jar的基本用法
@@ -17,6 +22,23 @@ public class PinYinExample {
 		String[] array=PinyinHelper.toGwoyeuRomatzyhStringArray('潘');
 		for (String string : array) {
 			System.out.println(string);
+		}
+	}
+	
+	@Test
+	public void TestToString(){
+		HanyuPinyinOutputFormat outFormat = new HanyuPinyinOutputFormat();  
+        outFormat.setCaseType(HanyuPinyinCaseType.UPPERCASE);//控制拼音的大小写
+        //是否带声调WITH_TONE_NUMBER：带声调 PAN1 LIANG2 FENG1
+        //WITH_TONE_MARK 用声调符号表示
+        outFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        //特殊拼音u的显示格式
+        outFormat.setVCharType(HanyuPinyinVCharType.WITH_V);
+		try {
+			System.out.println(PinyinHelper.toHanYuPinyinString("潘良烽", outFormat, " ",true));
+		} catch (BadHanyuPinyinOutputFormatCombination e) {
+			// TODO Auto-generated catch block 
+			e.printStackTrace();
 		}
 	}
 }
