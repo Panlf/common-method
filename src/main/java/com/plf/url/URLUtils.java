b75d700e4f959273956847c64bbe03f13497635f
Panlf <liangfeng_pan@163.com>
2017-09-05 20:31:53 +0800

    add urlutils

--- src/main/java/com/plf/url/URLUtils.java
+++ src/main/java/com/plf/url/URLUtils.java
@@ -1,5 +1,13 @@
 package com.plf.url;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * URL的工具类
  * @author plf 2017年9月5日下午8:11:02
@@ -7,4 +15,89 @@ package com.plf.url;
  */
 public class URLUtils {
 	
+	public String sendGet(String path){
+		String result="";
+		BufferedReader buff=null;
+		try {
+			URL url=new URL(path);
+			URLConnection conn=url.openConnection();
+			//设置通用属性
+			conn.setRequestProperty("Accept", "*/*");
+			conn.setRequestProperty("Connection", "Keep-Alive");
+			conn.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
+			conn.setRequestProperty("Accept-Language", "zh-CN,zh;q=0.8");
+			
+			conn.connect();
+			
+			InputStream ins=conn.getInputStream();
+			InputStreamReader insread=new InputStreamReader(ins,"utf-8");
+			buff=new BufferedReader(insread);
+			String line;
+			while((line=buff.readLine())!=null){
+				result += line;
+			}
+			return result;
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(buff!=null){
+				try {
+					buff.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+	
+	public String sendPost(String path){
+		String result="";
+		BufferedReader buff=null;
+		try {
+			URL url=new URL(path);
+			URLConnection conn=url.openConnection();
+			//设置通用属性
+			conn.setRequestProperty("Accept", "*/*");
+			conn.setRequestProperty("Connection", "Keep-Alive");
+			conn.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
+			conn.setRequestProperty("Accept-Language", "zh-CN,zh;q=0.8");
+			
+			 // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+			
+			conn.connect();
+			
+			InputStream ins=conn.getInputStream();
+			InputStreamReader insread=new InputStreamReader(ins,"utf-8");
+			buff=new BufferedReader(insread);
+			String line;
+			while((line=buff.readLine())!=null){
+				result += line;
+			}
+			return result;
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(buff!=null){
+				try {
+					buff.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
 }
