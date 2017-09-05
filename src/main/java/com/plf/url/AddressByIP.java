22e1e3a400d2b834af0f499e64bd62ad26fd0f90
Panlf <liangfeng_pan@163.com>
2017-09-05 20:16:25 +0800

    add getAddressByIP

---
+++ src/main/java/com/plf/url/AddressByIP.java
@@ -0,0 +1,47 @@
+package com.plf.url;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+public class AddressByIP {
+
+	public static void main(String[] args) {
+		System.out.println(new AddressByIP().getAddressByIP("39.182.67.245"));
+	}
+
+	private JSONObject getAddressByIP(String ip){
+		//使用淘宝IP库
+		String path="http://ip.taobao.com/service/getIpInfo.php?ip="+ip;
+		JSONObject data=null;
+		try {
+			URL url=new URL(path);
+			URLConnection conn=url.openConnection();
+			conn.connect();
+			InputStream ins=conn.getInputStream();
+			InputStreamReader insread=new InputStreamReader(ins,"utf-8");
+			BufferedReader buff=new BufferedReader(insread);
+			String nextline=buff.readLine();
+			if(nextline != null){
+				System.out.println(nextline);
+				data=JSON.parseObject(nextline);
+				System.out.println("JSON DATA===>"+data);
+			}
+			return data;
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return data;
+	}
+}
---
+++ src/main/java/com/plf/url/URLUtils.java
@@ -0,0 +1,10 @@
+package com.plf.url;
+
+/**
+ * URL的工具类
+ * @author plf 2017年9月5日下午8:11:02
+ *
+ */
+public class URLUtils {
+	
+}
